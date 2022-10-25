68b408531dbf0b08b2e279fdfd800bd426c28e1b
piyush_29 <piyush1125kumar@gmail.com>
2022-10-25 18:15:02 +0530

    Adding path variable logic

--- restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/helloworld/HelloWorldController.java
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
@@ -12,13 +13,22 @@ public class HelloWorldController {
     //return "Hello World"
 
     @GetMapping(path = "/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    //path parameter
+    ///users/{id}/todos/{id} =? /users/1/todos/200
+    //hello-world/path-variable/{name}
+    //hello-world/path-variable/{Piyush}
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
