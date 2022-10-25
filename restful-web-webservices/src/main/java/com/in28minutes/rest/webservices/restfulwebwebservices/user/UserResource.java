c770381fb0bdf8ca29fde1fccba225fbb2dcf39c
piyush_29 <piyush1125kumar@gmail.com>
2022-10-25 19:40:02 +0530

    Add exception handling and return 404 user not found

--- restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/user/UserResource.java
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/user/UserResource.java
@@ -22,7 +22,12 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return userDaoService.findOne(id);
+
+        User user = userDaoService.findOne(id);
+            if(user ==null)
+                throw new UserNotFoundException("id: "+id);
+        return user;
+
     }
 
     //POST /user
