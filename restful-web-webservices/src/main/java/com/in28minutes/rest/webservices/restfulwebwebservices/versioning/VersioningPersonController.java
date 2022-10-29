710a7ed10d26604f2c8ef60115379aa1d03ed1fe
piyush_29 <piyush1125kumar@gmail.com>
2022-10-29 22:31:20 +0530

    Request parameter versioning
    Custom header versioning
    
    Request Header

--- restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/versioning/VersioningPersonController.java
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/versioning/VersioningPersonController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebwebservices.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,28 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson(){
+        return new PersonV2(new Name("Piyush", "Kumar"));
+    }
+
+    @GetMapping(path="/person",params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParamter(){
+        return new PersonV1("Piyush Kumar");
+    }
+
+    @GetMapping(path="/person",params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParamter(){
+        return new PersonV2(new Name("Piyush", "Kumar"));
+    }
+
+    //Custom head versioning
+
+    @GetMapping(path="/person/header",headers = "X-API-Version=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader(){
+        return new PersonV1("Piyush Kumar");
+    }
+
+    @GetMapping(path="/person/header",headers = "X-API-Version=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader(){
         return new PersonV2(new Name("Piyush", "Kumar"));
     }
 }
