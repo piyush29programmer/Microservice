c52524127fa89dc5e2d02a5b3693784c52cda929
piyush_29 <piyush1125kumar@gmail.com>
2022-10-29 22:02:43 +0530

    Internationalisation
    Add support for en, fr and en language support message.
    Use LocaleContextHolder.getLocale() to get locale from request and read properties using MessageSource messageSource;

--- restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/helloworld/HelloWorldController.java
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //Rest API
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     //hello-world
 
     //return "Hello World"
@@ -30,4 +39,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-Internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.messgae",null,"Degault Message",locale);
+        //return "Hello World V2";
+    }
+
 }
