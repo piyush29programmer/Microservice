baf56ca9e640b148b773068b10c059881fb044b4
piyush_29 <piyush1125kumar@gmail.com>
2022-10-29 22:20:37 +0530

    Versioning REST API using the URI create different url for different version.

---
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/versioning/Name.java
@@ -0,0 +1,31 @@
+package com.in28minutes.rest.webservices.restfulwebwebservices.versioning;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+
+}
