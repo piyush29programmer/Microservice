924a754084857bca89abb749e9cac3d29d88d32d
piyush_29 <piyush1125kumar@gmail.com>
2022-11-03 10:53:27 +0530

    Using Hatoes we can send link in response to user .

--- restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/user/UserResource.java
+++ restful-web-webservices/src/main/java/com/in28minutes/rest/webservices/restfulwebwebservices/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +25,19 @@ public class UserResource {
         return userDaoService.findAll();
     }
 
+    //EntityModel
+    //WebMvcBuilder
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
 
         User user = userDaoService.findOne(id);
             if(user ==null)
                 throw new UserNotFoundException("id: "+id);
-        return user;
+
+            EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link =  WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
 
     }
 
